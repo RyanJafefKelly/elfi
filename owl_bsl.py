78a84eb2cede55924e3b6895e33fdb9ee70a91ec
Ryan Kelly <ryan@kiiii.com>
2024-03-28 18:17:42 +1000

    Owl ABC and BSL updates

--- owl_bsl.py
+++ owl_bsl.py
@@ -4,7 +4,10 @@ import elfi
 import matplotlib.pyplot as plt
 from elfi.examples import owl
 from elfi.methods.bsl import pre_sample_methods, pdf_methods
+from elfi.clients.multiprocessing import Client as MultiprocessingClient
 from ctypes import cdll
+import multiprocessing as mp
+import pickle as pkl
 
 # import rasterio
 import pandas as pd
@@ -30,6 +33,10 @@ def run_owl():
     ind_data_centroid = pd.read_csv("individual_data/individual_data/calibration_2011VA0533_centroid.csv")
 
     # true_params = np.array([2, 1.5, 1, 5])  # TODO
+
+    elfi.set_client(MultiprocessingClient(num_processes=6))  # TODO: Hardcoded
+
+
     m = owl.get_model(seed_obs=123, observed=False)
     batch_size = 50
 
@@ -43,7 +50,7 @@ def run_owl():
     likelihood = pdf_methods.semiparametric_likelihood()
 
     n_sim_round = 300
-    batch_size = 300
+    # batch_size = 300
     semi_bsl = elfi.BSL(m, n_sim_round=n_sim_round,
                         batch_size=batch_size, seed=123, likelihood=likelihood)
 
@@ -53,7 +60,7 @@ def run_owl():
     lmda_r = 5.0
     params = {'k': k, 'lmda_r': lmda_r, 'rho': rho, 'tau': tau}
 
-    mcmc_iterations = 2000  # sample size
+    mcmc_iterations = 20  # sample size
     est_post_cov = np.array([[0.02, 0.01], [0.01, 0.02]])  # covariance matrix for the proposal distribution
     logit_transform_bound = np.array([
                                     [0.1, 4],  # k
@@ -62,21 +69,29 @@ def run_owl():
                                     [0.1, 3.5]  # tau
                                    ])
 
+    sigma_proposals = np.array([[ 1.82484162,  0.04122204, -0.22807996, -0.05305404],
+       [ 0.04122204,  3.41680703, -0.15229068,  0.17158555],
+       [-0.22807996, -0.15229068,  2.46904036,  0.0493929 ],
+       [-0.05305404,  0.17158555,  0.0493929 ,  2.54673614]])
+
+
     res = semi_bsl.sample(mcmc_iterations,
-                          sigma_proposals=0.1*np.eye(4),
+                          sigma_proposals=sigma_proposals,  # TODO: check if works with transform...
                           params0=np.array([k, lmda_r, rho, tau]),
                           param_names=['k', 'lmda_r', 'rho', 'tau'],
                           logit_transform_bound=logit_transform_bound,
                           )
 
     print(res)
-    print(res.compute_ess())
-    res.plot_traces();
-    plt.savefig("owl_traces.png")
+    with open("owl_semibsl.pkl", "wb") as f:
+        pkl.dump(res, f)
+    # print(res.compute_ess())
+    # res.plot_traces();
+    # plt.savefig("owl_traces.png")
 
-    res.plot_pairs(reference_values=params,
-                   bins=30);
-    plt.savefig("owl_pairs.png")
+    # res.plot_pairs(reference_values=params,
+    #                bins=30);
+    # plt.savefig("owl_pairs.png")
     # owl_bsl_sampler = elfi.BSL(m['SL'], batch_size=batch_size, seed=123)
 
     # owl_bsl_sampler.plot_summary_statistics(batch_size=100, theta_point=true_params)
